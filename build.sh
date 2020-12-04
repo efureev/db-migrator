d2afe114f77329c642776a3a027e9b1fe94e9c13
Евгений Фуреев <efureev@yandex.ru>
2020-12-04 09:26:21 +0500

    fix

--- build.sh
+++ build.sh
@@ -2,18 +2,23 @@
 
 BUILD_PATH="build"
 APP_NAME="migrator"
+VERSION_BUILD=$(git log --pretty="%h" -n1 HEAD)
+VERSION_TAG=$(git log --pretty="%h" -n1 HEAD)
 
-for OS in darwin linux ;
-do
-    for ARCH in amd64 ;
-        do
-            ARCHX=x86
-            if [ $ARCH == "amd64" ]
-            then
-                ARCHX=x64
-            fi
-            echo "Building -> OS: $OS ARCH: $ARCH file: $APP_NAME.$OS.$ARCHX" ;
-            GOOS=$OS GOARCH=$ARCH go build -o $BUILD_PATH/$APP_NAME.$OS.$ARCHX ;
-        done
-done
-echo ""
+echo $VERSION_BUILD ;
+
+
+# for OS in darwin linux ;
+# do
+#     for ARCH in amd64 ;
+#         do
+#             ARCHX=x86
+#             if [ $ARCH == "amd64" ]
+#             then
+#                 ARCHX=x64
+#             fi
+#             echo "Building -> OS: $OS ARCH: $ARCH file: $APP_NAME.$OS.$ARCHX" ;
+#             GOOS=$OS GOARCH=$ARCH go build -o $BUILD_PATH/$APP_NAME.$OS.$ARCHX ;
+#         done
+# done
+# echo ""
