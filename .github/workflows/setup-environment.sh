5c34fd8395f6ddd12c020ee25858c200d446b81a
Eugene Fureev <fureev.e@promail36.com>
2024-10-25 15:35:51 +0300

    ci: new ci

---
+++ .github/workflows/setup-environment.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -euxo pipefail
+
+# configure skopeo.
+install -d -m 755 /etc/containers/registries.conf.d
+cat >/etc/containers/registries.conf.d/localhost-5000.conf <<'EOF'
+[[registry]]
+location = 'localhost:5000'
+insecure = true
+EOF
+
+# configure.
+systemctl stop docker
+cat >/etc/docker/daemon.json <<'EOF'
+{
+    "experimental": false,
+    "debug": false,
+    "features": {
+        "buildkit": true
+    },
+    "log-driver": "journald",
+    "labels": [
+        "os=linux"
+    ],
+    "hosts": [
+        "unix://"
+    ]
+}
+EOF
+
+# start docker without any command line flags as its entirely configured from daemon.json.
+install -d /etc/systemd/system/docker.service.d
+cat >/etc/systemd/system/docker.service.d/override.conf <<'EOF'
+[Service]
+ExecStart=
+ExecStart=/usr/bin/dockerd
+EOF
+
+# start.
+systemctl daemon-reload
+systemctl start docker
+
+# show information.
+docker info
+
+# install dependencies.
+apt-get update
+apt-get install -y qemu-user-static httpie
+
+# start a local registry.
+docker run -d --restart=unless-stopped --name registry -p 5000:5000 registry:2.8.3
+docker exec registry registry --version
--- build.sh
+++ build.sh
@@ -1,34 +1,28 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-BUILD_FOR_DOCKER=${BUILD_FOR_DOCKER}
-BUILD_PATH=${BUILD_PATH:-"build"}
-APP_NAME=${APP_NAME:-'migrator'}
-VERSION_BUILD=$(git log --pretty="%h" -n1 HEAD)
+set -eux
+
+if [ "$BUILDPLATFORM" != "$TARGETPLATFORM" ]; then
+    echo "Cross-compiling to $TARGETPLATFORM"
+    # $TARGETPLATFORM is something like:
+    #   linux/amd64
+    #   linux/arm64
+    #   linux/arm/v8
+    target_platform=(${TARGETPLATFORM//\// })
+    export GOOS=${target_platform[0]}
+    export GOARCH=${target_platform[1]}
+    if [ "${#target_platform[@]}" -gt 2 ]; then
+        export GOARM=${target_platform[2]//v}
+    fi
+else
+    echo "Compiling to $TARGETPLATFORM"
+fi
+
 VERSION_TAG=$(git describe --abbrev=0 --tags)
+VERSION_BUILD=$(git log --pretty="%h" -n1 HEAD)
 BUILD_TIME_LOCAL=$(date -u '+%Y-%m-%d_%H:%M:%S')
-
-VERSION=${VERSION:-$VERSION_TAG}
 BUILD_TIME=${BUILD_TIME:-$BUILD_TIME_LOCAL}
 
-# This list is based on:
-# https://github.com/golang/go/blob/master/src/go/build/syslist.go
-gooses=(
-    darwin linux #windows
-)
-gooses=($(printf -- '%s\n' "${gooses[@]}" | sort))
-
-# This list is based on:
-# https://github.com/golang/go/blob/master/src/go/build/syslist.go
-goarches=(
-    amd64 arm64
-)
-goarches=($(printf -- '%s\n' "${goarches[@]}" | sort))
-
-echo "Building options"
-echo "- VERSION: $VERSION"
-echo "- COMMIT: $VERSION_BUILD"
-echo "- BUILD_TIME: $BUILD_TIME"
-echo " "
 
 BUILDING_FLAGS="\
      -X 'migrator/src/commands.version=$VERSION_TAG' \
@@ -40,28 +34,4 @@ if [ "$BUILD_FOR_DOCKER" == '1' ]; then
   BUILDING_FLAGS="$BUILDING_FLAGS -s -w"
 fi
 
-for goos in "${gooses[@]}"
-do
-    for goarch in "${goarches[@]}"
-    do
-#        GOOS="$goos" GOARCH="$goarch" go build -o /dev/null main.go >out.log 2>err.log
-        CGO_ENABLED=0 GOOS="$goos" GOARCH="$goarch" go build -ldflags="$BUILDING_FLAGS" -o $BUILD_PATH/$APP_NAME.$goos.$goarch main.go >out.log 2>err.log
-        if [ $? -eq 0 ]
-        then
-            :
-        else
-            if grep -qe '^cmd/go: unsupported GOOS/GOARCH pair' err.log
-            then
-                :
-            else
-                mv err.log $goos-$goarch.err.log
-            fi
-        fi
-        if [ -s out.log ]
-        then
-            mv out.log $goos-$goarch.out.log
-        fi
-    done
-done
-rm -f out.log err.log
-echo ""
+CGO_ENABLED=0 go build -ldflags="$BUILDING_FLAGS"
