3a550ac6d43fb554a5a30bb2012b20c1b2661f71
Eugene Fureev <fureev.e@promail36.com>
2024-10-25 12:14:08 +0300

    fix

--- build.sh
+++ build.sh
@@ -10,6 +10,20 @@ BUILD_TIME_LOCAL=$(date -u '+%Y-%m-%d_%H:%M:%S')
 VERSION=${VERSION:-$VERSION_TAG}
 BUILD_TIME=${BUILD_TIME:-$BUILD_TIME_LOCAL}
 
+# This list is based on:
+# https://github.com/golang/go/blob/master/src/go/build/syslist.go
+gooses=(
+    darwin linux #windows
+)
+gooses=($(printf -- '%s\n' "${gooses[@]}" | sort))
+
+# This list is based on:
+# https://github.com/golang/go/blob/master/src/go/build/syslist.go
+goarches=(
+    amd64 arm64
+)
+goarches=($(printf -- '%s\n' "${goarches[@]}" | sort))
+
 echo "Building options"
 echo "- VERSION: $VERSION"
 echo "- COMMIT: $VERSION_BUILD"
@@ -26,19 +40,28 @@ if [ "$BUILD_FOR_DOCKER" == '1' ]; then
   BUILDING_FLAGS="$BUILDING_FLAGS -s -w"
 fi
 
-for OS in darwin linux; do # for OS in darwin ;
-  for ARCH in amd64; do
-    ARCHX=x86
-    if [ $ARCH == "amd64" ]; then
-      ARCHX=x64
-    fi
-    echo "Building -> OS: $OS ARCH: $ARCH file: $APP_NAME.$OS.$ARCHX"
-
-    # https://pkg.go.dev/cmd/link
-    CGO_ENABLED=0 GOOS=$OS GOARCH=$ARCH go build -ldflags="$BUILDING_FLAGS" \
-      -tags "postgres pg migrate migrator" \
-      -o $BUILD_PATH/$APP_NAME.$OS.$ARCHX
-
-  done
+for goos in "${gooses[@]}"
+do
+    for goarch in "${goarches[@]}"
+    do
+#        GOOS="$goos" GOARCH="$goarch" go build -o /dev/null main.go >out.log 2>err.log
+        CGO_ENABLED=0 GOOS="$goos" GOARCH="$goarch" go build -ldflags="$BUILDING_FLAGS" -o $BUILD_PATH/$APP_NAME.$goos.$goarch main.go >out.log 2>err.log
+        if [ $? -eq 0 ]
+        then
+            :
+        else
+            if grep -qe '^cmd/go: unsupported GOOS/GOARCH pair' err.log
+            then
+                :
+            else
+                mv err.log $goos-$goarch.err.log
+            fi
+        fi
+        if [ -s out.log ]
+        then
+            mv out.log $goos-$goarch.out.log
+        fi
+    done
 done
+rm -f out.log err.log
 echo ""
